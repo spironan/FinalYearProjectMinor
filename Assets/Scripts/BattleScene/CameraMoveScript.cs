b0b7d1db14346abb8abb7d68a1d412bdcdb118df
spironan <tecklee.deyi@gmail.com>
2017-04-24 22:03:32 +0800

    Initial Push With Basic Framework scenes done by me

---
+++ Assets/Scripts/BattleScene/CameraMoveScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMoveScript : MonoBehaviour
+{
+    Vector3 movement;
+    void Start()
+    {
+        movement.x = 1;
+        movement.y = 0;
+        movement.z = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            this.transform.position -= movement;
+
+        if (Input.GetKey(KeyCode.D))
+            this.transform.position += movement;
+    }
+}
---
+++ Assets/Scripts/BattleScene/ParallaxScrollingScript.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScrollingScript : MonoBehaviour 
+{
+    public bool hasParallax, canScroll;
+    public float backgroundSize;
+    public float parallaxSpeed;
+    public Camera camera;
+
+    private Transform cameraTransform;
+    private Transform[] layers;
+    private float viewZone = 10;
+    private int leftIndex;
+    private int rightIndex;
+    private float lastCameraX;
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (camera == null)
+            camera = Camera.main;
+
+        cameraTransform = camera.transform;
+        viewZone = camera.orthographicSize;
+        
+        layers = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; ++i)
+            layers[i] = transform.GetChild(i);
+
+        leftIndex = 0;
+        rightIndex = layers.Length - 1;
+        
+        if (parallaxSpeed > 0)
+            hasParallax = true;
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (hasParallax)
+        {
+            float deltaX = cameraTransform.position.x - lastCameraX;
+            transform.position += Vector3.right * (deltaX * parallaxSpeed);
+        }
+
+        lastCameraX = cameraTransform.position.x;
+
+        if (canScroll)
+        {
+            if (cameraTransform.position.x < (layers[leftIndex].transform.position.x + viewZone))
+                ScrollLeft();
+
+            if (cameraTransform.position.x > (layers[rightIndex].transform.position.x - viewZone))
+                ScrollRight();
+        }
+        
+    } 
+	
+    private void ScrollLeft()
+    {
+        int lastRight = rightIndex;
+        layers[rightIndex].position = Vector3.right * (layers[leftIndex].position.x - backgroundSize);
+        leftIndex = rightIndex;
+        rightIndex--;
+        if (rightIndex < 0)
+            rightIndex = layers.Length - 1;
+    }
+
+    private void ScrollRight()
+    {
+        int lastleft = leftIndex;
+        layers[leftIndex].position = Vector3.right * (layers[rightIndex].position.x + backgroundSize);
+        rightIndex = leftIndex;
+        leftIndex++;
+        if (leftIndex == layers.Length)
+            leftIndex = 0;
+    }
+}
+
