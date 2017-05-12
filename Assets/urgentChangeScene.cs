8b04304d3712ee3087488b279df8414a380c63f4
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2017-05-12 23:05:33 +0800

    main menu to do an urgent scene transition :V

---
+++ Assets/urgentChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class urgentChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        LoadingScreenManager.LoadScene("CharacterSelectScene");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
