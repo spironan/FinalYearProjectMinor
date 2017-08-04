de3027aa12a4fc21d179249169e52de39f555cbc
Li Zhaoyuan <lizhaoyuan123@hotmail.com>
2017-08-04 21:05:23 +0800

    video for the video scene

---
+++ Assets/VideoPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VideoPlayer : MonoBehaviour {
+    public MovieTexture movie;
+    RawImage rawImage;
+	// Use this for initialization
+	void Start () {
+        rawImage = GetComponent<RawImage>();
+        rawImage.texture = movie;
+        movie.Play();
+        movie.loop = true;
+    }
+}
